06d9829db497a0e3da5b39a2281c06bbe0b18045
Stephen McGuckin <stephendmcguckin@gamil.com>
2019-08-30 21:11:44 -0700

    Refactored jest configs to be json. These files inflate github's metrics saying this repo is more js than ts

--- rune2e.sh
+++ rune2e.sh
@@ -5,5 +5,5 @@ set -e
 npm run start &
 PROCESS_PID=$!
 sleep 0.5
-PORT=8080 npm run test -- -c './jest.e2e.config.js' || true
+PORT=8080 npm run test -- -c './jest.e2e.config.json' || true
 kill -9 "$PROCESS_PID"
